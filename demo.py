486190cf37d5539c9d4f95f71d0bdf9cb55f95a6
brighter eyes lynxes <81421921+brightereyeslynxes@users.noreply.github.com>
2021-08-08 18:14:57 +0100

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,39 +1,40 @@
-#!/usr/bin/env python3
+import cv2
 
 from BlazeposeRenderer import BlazeposeRenderer
+from CorpusLabeGame import CorpusLabe
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--edge', action="store_true",
                     help="Use Edge mode (postprocessing runs on the device)")
-parser_tracker = parser.add_argument_group("Tracker arguments")                 
-parser_tracker.add_argument('-i', '--input', type=str, default="rgb", 
+parser.add_argument('-i', '--input', type=str, default="rgb", 
                     help="'rgb' or 'rgb_laconic' or path to video/image file to use as input (default=%(default)s)")
-parser_tracker.add_argument("--pd_m", type=str,
+parser.add_argument("--pd_m", type=str,
                     help="Path to an .blob file for pose detection model")
-parser_tracker.add_argument("--lm_m", type=str,
-                    help="Landmark model ('full' or 'lite' or 'heavy') or path to an .blob file")
-parser_tracker.add_argument('-xyz', '--xyz', action="store_true", 
-                    help="Get (x,y,z) coords of reference body keypoint in camera coord system (only for compatible devices)")
-parser_tracker.add_argument('-c', '--crop', action="store_true", 
+parser.add_argument("--lm_m", type=str,
+                    help="Landmark model ('full' or 'lite' or '831') or path to an .blob file (default=%(default)s)")
+parser.add_argument('-c', '--crop', action="store_true", 
                     help="Center crop frames to a square shape before feeding pose detection model")
-parser_tracker.add_argument('--no_smoothing', action="store_true", 
+parser.add_argument('--no_smoothing', action="store_true", 
                     help="Disable smoothing filter")
-parser_tracker.add_argument('-f', '--internal_fps', type=int, 
+parser.add_argument('--filter_window_size', type=int, default=5,
+                    help="Smoothing filter window size. Higher value adds to lag and to stability (default=%(default)i)")                    
+parser.add_argument('--filter_velocity_scale', type=float, default=10,
+                    help="Smoothing filter velocity scale. Lower value adds to lag and to stability (default=%(default)s)")                    
+parser.add_argument('-f', '--internal_fps', type=int, 
                     help="Fps of internal color camera. Too high value lower NN fps (default= depends on the model)")                    
-parser_tracker.add_argument('--internal_frame_height', type=int, default=640,                                                                                    
+parser.add_argument('--internal_frame_height', type=int, default=640,                                                                                    
                     help="Internal color camera frame height in pixels (default=%(default)i)")                    
-parser_tracker.add_argument('-s', '--stats', action="store_true", 
+parser.add_argument('-s', '--stats', action="store_true", 
                     help="Print some statistics at exit")
-parser_tracker.add_argument('-t', '--trace', action="store_true", 
+parser.add_argument('-t', '--trace', action="store_true", 
                     help="Print some debug messages")
-parser_tracker.add_argument('--force_detection', action="store_true", 
+parser.add_argument('--force_detection', action="store_true", 
                     help="Force person detection on every frame (never use landmarks from previous frame to determine ROI)")
 
-parser_renderer = parser.add_argument_group("Renderer arguments")
-parser_renderer.add_argument('-3', '--show_3d', choices=[None, "image", "world", "mixed"], default=None,
-                    help="Display skeleton in 3d in a separate window. See README for description.")
-parser_renderer.add_argument("-o","--output",
+parser.add_argument('-3', '--show_3d', action="store_true", 
+                    help="Display skeleton in 3d in a separate window (valid only for full body landmark model)")
+parser.add_argument("-o","--output",
                     help="Path to output video file")
  
 
@@ -43,31 +44,53 @@ if args.edge:
     from BlazeposeDepthaiEdge import BlazeposeDepthai
 else:
     from BlazeposeDepthai import BlazeposeDepthai
-tracker = BlazeposeDepthai(input_src=args.input, 
+pose = BlazeposeDepthai(input_src=args.input, 
             pd_model=args.pd_m,
             lm_model=args.lm_m,
-            smoothing=not args.no_smoothing,   
-            xyz=args.xyz,            
+            smoothing=not args.no_smoothing,
+            filter_window_size=args.filter_window_size,
+            filter_velocity_scale=args.filter_velocity_scale,                
             crop=args.crop,
             internal_fps=args.internal_fps,
             internal_frame_height=args.internal_frame_height,
             force_detection=args.force_detection,
-            stats=True,
+            stats=args.stats,
             trace=args.trace)   
 
 renderer = BlazeposeRenderer(
-                tracker, 
+                pose, 
                 show_3d=args.show_3d, 
                 output=args.output)
 
+game = CorpusLabe(
+                pose)
+
 while True:
     # Run blazepose on next frame
-    frame, body = tracker.next_frame()
+    frame, body = pose.next_frame()
     if frame is None: break
     # Draw 2d skeleton
     frame = renderer.draw(frame, body)
-    key = renderer.waitKey(delay=1)
+    frame = game.draw(frame, body)
+    key = renderer.waitKey(frame, delay=1)
     if key == 27 or key == ord('q'):
         break
+    #elif key == ord('a'):
+    #    game.show_angle = not game.show_angle
+    elif key == ord('g'): # either game or acquire angle measures
+        game.play_game = not game.play_game
+        game.show_angle = not game.show_angle
+    #elif key == ord('p'): #
+    #    game.current_exercise = abs(game.current_exercise - 1)
+    #    game.current_angle[game.current_exercise] = 0
+    #    game.current_max_angle[game.current_exercise] = 0
+    elif key == ord('r'):
+        game.current_max_angle[game.current_exercise] = 0
+        game.current_max_angle[abs(game.current_exercise - 1)] = 0
+    elif key == ord('h'):
+        game.show_help = not game.show_help
+    elif key == ord('s'):
+        game.save_data()
+
 renderer.exit()
-tracker.exit()
+pose.exit()
